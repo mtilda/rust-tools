65c3b1802dc572d345a779f3f8f1b953bfc626a8
Mathilda <me@mathilda.dev>
2024-05-30 13:04:46 -0400

    Add function `subtract`

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn subtract(left: usize, right: usize) -> usize {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -17,9 +21,13 @@ mod tests {
         assert_eq!(result, expected);
     }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[rstest]
+    #[case((0, 0), 0)]
+    #[case((1, 1), 0)]
+    #[case((123, 1), 122)]
+    fn test_subtract(#[case] params: (usize, usize), #[case] expected: usize) {
+        let (left, right) = params;
+        let result = subtract(left, right);
+        assert_eq!(result, expected);
     }
 }
