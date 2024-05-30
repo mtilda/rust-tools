3ce5a9e50c210504d936f2d7c69e5e835898fc4f
Mathilda <me@mathilda.dev>
2024-05-30 13:04:27 -0400

    Add crate `rstest`

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,17 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case((0, 0), 0)]
+    #[case((1, 1), 2)]
+    #[case((123, 456), 579)]
+    fn test_add(#[case] params: (usize, usize), #[case] expected: usize) {
+        let (left, right) = params;
+        let result = add(left, right);
+        assert_eq!(result, expected);
+    }
 
     #[test]
     fn it_works() {
